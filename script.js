3e9141b2437b6b46dba41e7f6a8bfba92812ae61
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2024-05-15 13:29:34 -0600

    info.txt 5/15

--- script.js
+++ script.js
@@ -9,81 +9,102 @@ class Node {
   constructor(key, value) {
     this.key = key;
     this.value = value;
-    this.next = null; // If there is a nextNode then I need a head generated
-    // this.nextNode = nextNode;  //Not appropriate for this circumstance.
+    this.next = null; // It seems head will be generated automatically if needed. verify this.
   }
 }
 
 class HashMap {
-  constructor(key, value) {
-    this.buckets = new Array(16);
+  constructor() {
+    this.arrayLength = 16;
+    this.buckets = new Array(this.arrayLength);
     this.size = 0;
-    // this.head = null;  //preemptive placement is not best practices, rather address is in set() if/when it occurs.
   }
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
+
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
-      console.log("hash:", hashCode);
     }
-    return hashCode % this.buckets.length;
+    console.log("hash:", hashCode % this.arrayLength);
+    return hashCode % this.arrayLength;
+  }
+
+  resize() {
+    //resize when existing array becomes too small.
+    this.arrayLength = this.arrayLength * 2;
+    let tempBuckets = new Array(this.arrayLength);
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      let currentNode = this.buckets[i];
+      let previousNode = null;
+
+      while (currentNode !== null && currentNode !== undefined) {
+        while (currentNode !== null) {
+          if (previousNode !== null) {
+            previousNode.next = null; //detach previous node from current node
+          }
+          console.log(
+            "Resize: Temp",
+            tempBuckets,
+            "Current:",
+            myHashMap,
+            "size:",
+            this.size
+          );
+          console.log("Resize CurrentNode:", currentNode);
+
+          //rehash key and create newNode in larger array
+          let newHashCode = this.hash(currentNode.key);
+          let newNode = new Node(currentNode.key, currentNode.value);
+          this.size--;
+
+          // if (tempBuckets[newHashCode] === undefined) {
+          if (!tempBuckets[newHashCode]) {
+            tempBuckets[newHashCode] = newNode;
+            this.size++;
+          } else {
+            let currentTempNode = tempBuckets[newHashCode];
+            // while (currentNode.next !== null) {
+            while (currentNode.next) {
+              currentTempNode = currentTempNode.next; //code breaks here if linkedList is greater than 1.
+            }
+            currentTempNode.next = newNode;
+            this.size++;
+          }
+          previousNode = currentNode;
+          currentNode = currentNode.next;
+        }
+        this.buckets[i] = null;
+      }
+      this.buckets = tempBuckets;
+      console.log("Set new array length:", this.buckets.length);
+    }
   }
 
   set(key, value) {
     //creates a node
     let sizeFactor = 0.75;
+    const hashCode = this.hash(key);
+    const newNode = new Node(key, value);
 
     if (this.size / this.buckets.length >= sizeFactor) {
       console.log("Set: Array Length triggered");
-      let tempBuckets = new Array(this.buckets.length * 2);
-
-      for (let i = 0; i < this.buckets.length; i++) {
-        let currentNode = this.buckets[i];
-        let previousNode = null;
-
-        while (currentNode !== null) {
-          //detach current node from linked list
-          if (previousNode !== null) {
-            previousNode.next = null; //detach previous node from current node
-          }
-          console.log('MyHashMap', myHashMap)
-          console.log('Resize CurrentNode:', currentNode);
-          // create new hash, new Node and assign to new array
-          let newHashCode = this.hash(currentNode.key); //rehash key
-          let newNode = new Node(currentNode.key, currentNode.value);
-
-          if ((tempBuckets[newHashCode] === undefined)) {
-            tempBuckets[newHashCode] = newNode;
-          } else {
-            let currentTempNode = tempBuckets[newHashCode];
-            while (currentNode.next !== null) {
-              currentTempNode = currentTempNode.next;
-            }
-            currentTempNode.next = newNode;
-          }
-          previousNode = currentNode;
-          currentNode = currentNode.next;
-        }
-        this.buckets[i] = null; //all nodes reassigned, set bucket to empty.
-      }
-      this.buckets = tempBuckets;
-      console.log("Set new array length:", this.buckets.length);
+      this.resize();
     }
-
-    const hashCode = this.hash(key);
-    const newNode = new Node(key, value);
 
     if (!this.buckets[hashCode]) {
       this.buckets[hashCode] = newNode;
       this.size++;
       console.log("set:", hashCode, key, value, "size:", this.size);
+      console.log("Buckets:", this.buckets);
     } else {
       let currentNode = this.buckets[hashCode];
       while (currentNode.next) {
         currentNode = currentNode.next;
+        console.log("myHashMap:", myHashMap);
+        console.log("Buckets:", this.buckets);
       }
       currentNode.next = newNode;
       this.size++;
@@ -97,10 +118,6 @@ class HashMap {
       );
     }
   }
-
-  // this.buckets = new Node(key, value);
-  //assign hashCode to new Node(key, value) that gets assigned to a bucket.
-  //}
 
   get(key) {
     //return value or null
@@ -130,7 +147,8 @@ myHashMap.set("dingle", "bawlz");
 myHashMap.set("frez", "smyth");
 myHashMap.set("will", "smith");
 myHashMap.set("jimmy", "bird");
-myHashMap.set("fred", "bird");
+// myHashMap.set("fred", "bird");
+myHashMap.set("freddie", "bird");
 myHashMap.set("Evan", "Anderson");
 myHashMap.set("JC", "Anderson");
 myHashMap.set("niki", "Neeshan");
@@ -138,17 +156,3 @@ myHashMap.set("Jim", "Jones");
 console.log(myHashMap);
 myHashMap.set("Alicia", "Bitmore");
 console.log(myHashMap);
-
-//    //Collision management
-// class Node {
-//   constructor(value, nextNode) {
-//     this.value = value;
-//     this.nextNode = nextNode;
-//   }
-// }
-
-// class LinkedList {
-//   constructor() {
-//     this.head = null;
-//   }
-// }
